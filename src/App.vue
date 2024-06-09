a54695777548c477f6e3d1e68e9a77eb8d4bd47c
Alipa Gian Rafid <taiee8288@gmail.com>
2024-06-09 11:58:55 +0700

    add vercel.json

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <nav>
+    </nav>
+    <router-view/>
+  </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
@@ -21,6 +13,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import applyScrollEffects from './script.js';
+import './assets/css/tailwind.css'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
+applyScrollEffects();
